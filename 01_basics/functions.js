4c8fcb8a88207491cdfdfe30674a5bb3346d3cc3
shivangi Raj <97498067+Code4575@users.noreply.github.com>
2024-06-20 19:43:29 +0000

    functions

---
+++ 01_basics/functions.js
@@ -0,0 +1,33 @@
+function name(){
+    console.log("Shivangi");
+}
+
+//     name()      //execution
+
+function add(num1,num2){
+    console.log(num1+num2);
+}
+
+    // const result=add(3,5)
+    // console.log("result:",result);
+
+    function add1(num1,num2){
+    //    let result= num1+num2;
+    //    console.log("shivangi");                 //return ke baad ki koi bhi value print nahi hogi
+    //    return result
+
+    return num1+num2
+    }
+
+    const ans=add1(3,5)
+
+
+
+
+
+function username(username){
+return `${username} just logged in`
+}
+console.log(username("shivangi"))
+
+// ()   karne par undefined aayega
