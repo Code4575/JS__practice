424968fd988c9f6bfcf6367a74a7f7bf50c18795
shivangi Raj <97498067+Code4575@users.noreply.github.com>
2024-06-23 20:32:08 +0000

    control

---
+++ control_flow/control.js
@@ -0,0 +1,67 @@
+//if
+// == check
+// === check type  also
+
+// if(2 ==="2"){
+//     console.log("executes")
+// }
+
+
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+// const month=3
+// switch (month) {
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("feb");
+//         break;
+//     case 3:
+//         console.log("march");
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+// const usermail=""
+
+// if(usermail){
+//     console.log("got it");
+// }else{
+//     console.log("don't");
+// }
+
+
+//falsy value ----- false, 0, -0, BigInt (0n), null, "" ,undefined, NaN
+
+// truthy value------"0", 'false, " ", [], {}, function(){}
+
+// array ka size,,    object ki keys se check karna hai
+
+
+
+
+//nullish coalescing operator
+
+
+let val=5;
+val =5??10
+// val1=null??10    ------------   ans =10 also for undefined
+// val=undefined ?? 10 ?? 20  ------ gives 10 that mean gives first value
+console.log(val);
+
+// terniary operator is differnt from this   condition ? true : false
+
+
