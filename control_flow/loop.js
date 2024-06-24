c19441f738da208911ccf25045ffe44455150ac3
shivangi Raj <97498067+Code4575@users.noreply.github.com>
2024-06-24 14:14:22 +0000

    loop

---
+++ control_flow/loop.js
@@ -0,0 +1,38 @@
+// for (let i = 0; i < 5; i++) {
+//     console.log(`for i: ${i}`);
+// for (let j = 0; j < 5; j++) {
+//     console.log(`for j: ${i}`);
+    
+// }
+// }
+
+
+//            while loop
+
+// let index=0
+// while (index<=10) {
+//     console.log(`valuse is ${index}`);
+//     index=index+2;
+// }
+
+
+
+// let n=0
+// let array=["harry","potter","snape","Dumbledore"]
+// while (array.length>n) {
+//     console.log(`these are ${array[n]}`);
+// n++;
+// }
+
+
+
+//          do while
+
+
+// let score=1;
+// do {
+//     console.log(`the score is ${score}`);
+//     score=score+2;
+// } while (score<=10);
+
+
